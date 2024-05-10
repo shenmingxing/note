4c0c2299e506709e71dcc2f23520d4aaf9e4856d
smx <1547022735@qq.com>
2024-05-10 17:38:26 +0800

    all

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+// export default function Page() {
+//   return (
+//     <div>DashBorad Page</div>
+//   )
+// }
+
+
+export default function Page() {
+    return <p>Customers Page</p>;
+  }
